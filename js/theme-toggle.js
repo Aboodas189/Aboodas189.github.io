41ebfe6413fb5eb0d1ceb91e291a58a31504e2b8
Abdulelah Bin Elias Felemban <87627909+Aboodas189@users.noreply.github.com>
2024-10-08 03:32:57 +0300

    Create theme-toggle.js

---
+++ js/theme-toggle.js
@@ -0,0 +1,6 @@
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+    themeToggle.textContent = document.body.classList.contains('dark-theme') ? '🌞 Light Mode' : '🌜 Dark Mode';
+});
