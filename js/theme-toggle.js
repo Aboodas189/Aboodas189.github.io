7704b5b16cbf36f7dc91a81ed0856f2fe3e995ba
Abdulelah Bin Elias Felemban <87627909+Aboodas189@users.noreply.github.com>
2024-10-09 03:31:23 +0300

    Update theme-toggle.js

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,10 +1,4 @@
 const themeToggle = document.getElementById('theme-toggle');
-if (document.body.classList.contains('dark-theme')) {
-    themeToggle.textContent = '🌞 Light Mode';
-} else {
-    themeToggle.textContent = '🌜 Dark Mode';
-}
-
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
     if (document.body.classList.contains('dark-theme')) {
