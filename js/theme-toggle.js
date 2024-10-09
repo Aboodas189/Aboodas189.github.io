f4061400dde5e6bfa4bbe769fba94b09abc60b2c
Abdulelah Bin Elias Felemban <87627909+Aboodas189@users.noreply.github.com>
2024-10-09 03:51:59 +0300

    Update theme-toggle.js

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,25 +1,9 @@
 const themeToggle = document.getElementById('theme-toggle');
-
-// Check if a theme is stored in localStorage
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme) {
-    document.body.classList.add(savedTheme);
-    if (savedTheme === 'dark-theme') {
-        themeToggle.textContent = '🌞 Light Mode';
-    }
-}
-
-// Event listener for theme toggle button
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
-    
     if (document.body.classList.contains('dark-theme')) {
         themeToggle.textContent = '🌞 Light Mode';
-        localStorage.setItem('theme', 'dark-theme'); // Save theme to localStorage
     } else {
         themeToggle.textContent = '🌜 Dark Mode';
-        localStorage.setItem('theme', ''); // Remove theme from localStorage
     }
 });
-
-
